a4abb70c850895a1d05217e26b3dcdcae19669cc
FlorentLM <25004801+FlorentLM@users.noreply.github.com>
2025-03-20 03:42:01 +0000

    Added support for native ffmpeg binary

--- beetsplug/beetstream/stream.py
+++ beetsplug/beetstream/stream.py
@@ -1,31 +1,47 @@
 from beetsplug.beetstream.utils import path_to_content_type
-from flask import send_file, Response
-
+import flask
+import shutil
 import importlib
-have_ffmpeg = importlib.util.find_spec("ffmpeg") is not None
 
-if have_ffmpeg:
+ffmpeg_bin = shutil.which("ffmpeg") is not None
+ffmpeg_python = importlib.util.find_spec("ffmpeg") is not None
+
+if ffmpeg_python:
     import ffmpeg
+elif ffmpeg_bin:
+    import subprocess
 
-def send_raw_file(filePath):
-    return send_file(filePath, mimetype=path_to_content_type(filePath))
+have_ffmpeg = ffmpeg_python or ffmpeg_bin
 
-def transcode_and_stream(filePath, maxBitrate):
-    if not have_ffmpeg:
-        raise RuntimeError("Can't transcode, ffmpeg-python is not available")
 
-    outputStream = (
-        ffmpeg
-        .input(filePath)
-        .audio
-        .output('pipe:', format="mp3", audio_bitrate=maxBitrate * 1000)
-        .run_async(pipe_stdout=True, quiet=True)
-    )
+def direct(filePath):
+    return flask.send_file(filePath, mimetype=path_to_content_type(filePath))
 
-    return Response(outputStream.stdout, mimetype='audio/mpeg')
+def transcode(filePath, maxBitrate):
+    if ffmpeg_python:
+        output_stream = (
+            ffmpeg
+            .input(filePath)
+            .audio
+            .output('pipe:', format="mp3", audio_bitrate=maxBitrate * 1000)
+            .run_async(pipe_stdout=True, quiet=True)
+        )
+    elif ffmpeg_bin:
+        command = [
+            "ffmpeg",
+            "-i", filePath,
+            "-f", "mp3",
+            "-b:a", f"{maxBitrate}k",
+            "pipe:1"
+        ]
+        output_stream = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    else:
+        raise RuntimeError("Can't transcode, ffmpeg is not available.")
+
+    return flask.Response(output_stream.stdout, mimetype='audio/mpeg')
 
 def try_to_transcode(filePath, maxBitrate):
     if have_ffmpeg:
-        return transcode_and_stream(filePath, maxBitrate)
+        return transcode(filePath, maxBitrate)
     else:
-        return send_raw_file(filePath)
+        return direct(filePath)

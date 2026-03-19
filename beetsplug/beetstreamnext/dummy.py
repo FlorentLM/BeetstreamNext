de91f4fae334c2f69608a9ab226329b82e4b315f
FlorentLM <25004801+FlorentLM@users.noreply.github.com>
2026-03-19 00:12:07 +0000

    Added tokenInfo route to keep some clients happy

--- beetsplug/beetstreamnext/dummy.py
+++ beetsplug/beetstreamnext/dummy.py
@@ -1,7 +1,7 @@
 import flask
 
 from beetsplug.beetstreamnext import app
-from beetsplug.beetstreamnext.utils import subsonic_response
+from beetsplug.beetstreamnext.utils import subsonic_response, subsonic_error
 
 
 # Fake endpoint to avoid some apps errors
@@ -17,3 +17,25 @@ def scrobble():
 def ping():
     r = flask.request.values
     return subsonic_response({}, r.get('f', 'xml'))
+
+
+@app.route('/rest/tokenInfo', methods=["GET", "POST"])
+@app.route('/rest/tokenInfo.view', methods=["GET", "POST"])
+def token_info():
+    r = flask.request.values
+    resp_fmt = r.get('f', 'xml')
+
+    api_key = r.get('apiKey', '')
+    if not api_key:
+        return subsonic_error(10, resp_fmt=resp_fmt)
+
+    from beetsplug.beetstreamnext.authentication import get_user
+    import hashlib
+
+    api_key_hash = hashlib.sha256(api_key.encode('utf-8')).hexdigest()
+    username = get_user(api_key_hash)
+
+    if not username:
+        return subsonic_error(40, resp_fmt=resp_fmt)
+
+    return subsonic_response({'tokenInfo': {'username': username}}, resp_fmt)

43642d3906642de8495af3dc22587cbc843d2912
FlorentLM <25004801+FlorentLM@users.noreply.github.com>
2026-04-25 16:44:44 +0100

    Made CSRF default on, only exempting the REST endpoints

--- beetsplug/beetstreamnext/__init__.py
+++ beetsplug/beetstreamnext/__init__.py
@@ -2,7 +2,7 @@
 BeetstreamNext is a Beets.io plugin that exposes OpenSubsonic API endpoints.
 """
 
-from .application import app
+from .application import app, csrf
 
 # Register middleware with `before_request` and `after_request`
 from . import middleware  # noqa: F401
@@ -11,6 +11,7 @@ from . import middleware  # noqa: F401
 from .api import api_bp
 from .public import public_bp
 app.register_blueprint(api_bp)
+csrf.exempt(api_bp)
 app.register_blueprint(public_bp)
 
 # And import the beets hook

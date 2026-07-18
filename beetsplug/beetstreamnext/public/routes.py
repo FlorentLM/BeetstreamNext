8611af900fd5adb72c8f2a69f8dc4490c81bbfd4
FlorentLM <25004801+FlorentLM@users.noreply.github.com>
2026-07-18 01:56:42 +0100

    Added a couple of type hints

--- beetsplug/beetstreamnext/public/routes.py
+++ beetsplug/beetstreamnext/public/routes.py
@@ -1,3 +1,4 @@
+from typing import Tuple, Any
 from flask import render_template
 
 from . import public_bp
@@ -6,12 +7,12 @@ from beetsplug.beetstreamnext.utils.general import get_server_info
 
 
 @public_bp.route('/')
-def home():
+def home() -> str:
     stats = get_server_info(extended=False)
     stats['status'] = 'running'
     return render_template('index.html', stats=stats)
 
 
 @public_bp.app_errorhandler(404)
-def page_not_found(e):
+def page_not_found(_e: Any) -> Tuple[str, int]:
     return render_template('404.html'), 404

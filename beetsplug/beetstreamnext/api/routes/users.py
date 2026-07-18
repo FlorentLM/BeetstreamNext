f476c644cc486117ce6808231a46ef87c3ffc48a
FlorentLM <25004801+FlorentLM@users.noreply.github.com>
2026-07-18 02:37:58 +0100

    Cleaned up users management endpoints and added getAvatar endpoint

--- beetsplug/beetstreamnext/api/routes/users.py
+++ beetsplug/beetstreamnext/api/routes/users.py
@@ -2,41 +2,40 @@ import flask
 
 from .. import api_bp
 
-from beetsplug.beetstreamnext.schemas import ALL_USER_FIELDS, ALLOWED_BITRATES
-from beetsplug.beetstreamnext.core.users_crud import create_user, update_user, delete_user, get_userdata, load_all_users
+from beetsplug.beetstreamnext.schemas import ALLOWED_BITRATES, USER_ROLES_SCHEMA
 from beetsplug.beetstreamnext.utils.general import api_bool
 from beetsplug.beetstreamnext.utils.text import safe_str
 from beetsplug.beetstreamnext.api.responses import subsonic_response, subsonic_error
+from beetsplug.beetstreamnext.core.images import sniff_image
+from beetsplug.beetstreamnext.core.users_crud import (
+    create_user, update_user, delete_user, get_userdata, load_all_users, get_user_avatar
+)
 
 
 def user_payload(user_data: dict) -> dict:
-    """Build a Subsonic user dict from a load_userdata result."""
-    return {
-        'username':            user_data.get('username', ''),
-        'email':               user_data.get('email', '') or '',
-        'scrobblingEnabled':   bool(user_data.get('scrobblingEnabled', True)),
-        'maxBitRate':          int(user_data.get('maxBitRate', 0)),
-        'adminRole':           bool(user_data.get('adminRole', False)),
-        'settingsRole':        bool(user_data.get('settingsRole', True)),
-        'downloadRole':        bool(user_data.get('downloadRole', False)),
-        'uploadRole':          bool(user_data.get('uploadRole', False)),
-        'playlistRole':        bool(user_data.get('playlistRole', True)),
-        'coverArtRole':        bool(user_data.get('coverArtRole', False)),
-        'commentRole':         bool(user_data.get('commentRole', True)),
-        'podcastRole':         bool(user_data.get('podcastRole', False)),
-        'streamRole':          bool(user_data.get('streamRole', True)),
-        'jukeboxRole':         bool(user_data.get('jukeboxRole', False)),
-        'shareRole':           bool(user_data.get('shareRole', False)),
-        'videoConversionRole': bool(user_data.get('videoConversionRole', False)),
-        # 'avatarLastChanged':   '',  # TODO
-        'folder':              [0],     # Beets has only one 'folder'
+    """Build a OpenSubsonic user dict."""
+
+    payload = {
+        'username': user_data.get('username', ''),
+        'email': user_data.get('email') or '',
+        'maxBitRate': int(user_data.get('maxBitRate', 0)),
+        'folder': [0],  # Beets has only one 'folder'
     }
+
+    for field_name, _, _ in USER_ROLES_SCHEMA:
+        payload[field_name] = bool(user_data.get(field_name, False))
+
+    last_changed = user_data.get('avatarLastChanged')
+    if last_changed:
+        payload['avatarLastChanged'] = int(last_changed * 1000)
+
+    return payload
 
 
 # Spec: https://opensubsonic.netlify.app/docs/endpoints/getUser/
 @api_bp.route('/getUser', methods=["GET", "POST"])
 @api_bp.route('/getUser.view', methods=["GET", "POST"])
-def endpoint_get_user():
+def endpoint_get_user() -> flask.Response:
     r = flask.request.values
     resp_fmt = r.get('f', default='xml', type=safe_str)
     username = r.get('username', default=flask.g.username, type=safe_str)    # Required
@@ -65,7 +64,7 @@ def endpoint_get_user():
 # Spec: https://opensubsonic.netlify.app/docs/endpoints/getUsers/
 @api_bp.route('/getUsers', methods=["GET", "POST"])
 @api_bp.route('/getUsers.view', methods=["GET", "POST"])
-def endpoint_get_users():
+def endpoint_get_users() -> flask.Response:
     r = flask.request.values
     resp_fmt = r.get('f', default='xml', type=safe_str)
 
@@ -83,12 +82,11 @@ def endpoint_get_users():
 # Spec: https://opensubsonic.netlify.app/docs/endpoints/createUser/
 @api_bp.route('/createUser', methods=["GET", "POST"])
 @api_bp.route('/createUser.view', methods=["GET", "POST"])
-def endpoint_create_user():
+def endpoint_create_user() -> flask.Response:
     r = flask.request.values
     resp_fmt = r.get('f', default='xml', type=safe_str)
     username = r.get('username', default='', type=safe_str)         # Required
     password = r.get('password', default='', type=str)              # Required
-    # email = r.get('email', default='', type=safe_str)             # Required? yes but no
 
     if not flask.g.user_data or not bool(flask.g.user_data.get('adminRole')):
         return subsonic_error(50, resp_fmt=resp_fmt)
@@ -98,13 +96,16 @@ def endpoint_create_user():
 
     try:
         params = {}
-        for field in ALL_USER_FIELDS:
-            if field in r:
-                if field == 'maxBitRate':
-                    val = r.get(field, default=0, type=int)
-                    params[field] = val if val in ALLOWED_BITRATES else 0
-                else:
-                    params[field] = int(r.get(field, default=False, type=api_bool))
+        if 'maxBitRate' in r:
+            br = r.get('maxBitRate', default=0, type=int)
+            params['maxBitRate'] = br if br in ALLOWED_BITRATES else 0
+
+        if 'email' in r:
+            params['email'] = r.get('email', type=safe_str)
+
+        for role_name, _, _ in USER_ROLES_SCHEMA:
+            if role_name in r:
+                params[role_name] = api_bool(r.get(role_name))
 
         # Explicitly pull adminRole for create_user
         is_admin = params.pop('adminRole', False)
@@ -119,14 +120,11 @@ def endpoint_create_user():
 # Spec: https://opensubsonic.netlify.app/docs/endpoints/updateUser/
 @api_bp.route('/updateUser', methods=["GET", "POST"])
 @api_bp.route('/updateUser.view', methods=["GET", "POST"])
-def endpoint_update_user():
+def endpoint_update_user() -> flask.Response:
     r = flask.request.values
     resp_fmt = r.get('f', default='xml', type=safe_str)
-    username = r.get('username', default='', type=safe_str)     # Required
+    username = r.get('username', default=r.get('u', ''), type=safe_str)     # Required
     password = r.get('password', default='', type=str)
-    # email = r.get('email', default='', type=safe_str)
-
-    # TODO: This endpoint should not allow an admin to demote themselves
 
     if not flask.g.user_data or not bool(flask.g.user_data.get('adminRole')):
         return subsonic_error(50, resp_fmt=resp_fmt)
@@ -144,9 +142,17 @@ def endpoint_update_user():
             br = r.get('maxBitRate', default=0, type=int)
             updates['maxBitRate'] = br if br in ALLOWED_BITRATES else 0
 
-        for field in ALL_USER_FIELDS:
-            if field in r and field not in ('password', 'maxBitRate'):
-                updates[field] = int(r.get(field, default=False, type=api_bool))
+        if 'email' in r:
+            updates['email'] = r.get('email', type=safe_str)
+
+        for role_name, _, _ in USER_ROLES_SCHEMA:
+            if role_name in r:
+                val = api_bool(r.get(role_name))
+
+                if role_name == 'adminRole' and username == flask.g.username and val is False:
+                    return subsonic_error(50, message='You cannot revoke your own admin status.', resp_fmt=resp_fmt)
+
+                updates[role_name] = val
 
         update_user(username, **updates)
         return subsonic_response({}, resp_fmt)
@@ -158,7 +164,7 @@ def endpoint_update_user():
 # Spec: https://opensubsonic.netlify.app/docs/endpoints/deleteUser/
 @api_bp.route('/deleteUser', methods=["GET", "POST"])
 @api_bp.route('/deleteUser.view', methods=["GET", "POST"])
-def endpoint_delete_user():
+def endpoint_delete_user() -> flask.Response:
     r = flask.request.values
     resp_fmt = r.get('f', default='xml', type=safe_str)
     target_user = r.get('username', default='', type=safe_str)   # Required
@@ -170,7 +176,7 @@ def endpoint_delete_user():
         return subsonic_error(10, message='Username to be deleted must be passed.', resp_fmt=resp_fmt)
 
     if target_user == flask.g.username:
-        return subsonic_error(50, message="Admins cannot delete their own account via this endpoint.", resp_fmt=resp_fmt)
+        return subsonic_error(50, message='Admins cannot delete their own account via this endpoint.', resp_fmt=resp_fmt)
 
     if delete_user(target_user):
         return subsonic_response({}, resp_fmt)
@@ -181,7 +187,7 @@ def endpoint_delete_user():
 # Spec: https://opensubsonic.netlify.app/docs/endpoints/changePassword/
 @api_bp.route('/changePassword', methods=["GET", "POST"])
 @api_bp.route('/changePassword.view', methods=["GET", "POST"])
-def endpoint_change_password():
+def endpoint_change_password() -> flask.Response:
     r = flask.request.values
     resp_fmt = r.get('f', default='xml', type=safe_str)
     target_user = r.get('username', default=flask.g.username, type=safe_str)    # Required
@@ -202,3 +208,23 @@ def endpoint_change_password():
         return subsonic_response({}, resp_fmt)
     except ValueError:
         return subsonic_error(70, resp_fmt=resp_fmt)
+
+
+# Spec: https://opensubsonic.netlify.app/docs/endpoints/getAvatar/
+@api_bp.route('/getAvatar', methods=["GET"])
+@api_bp.route('/getAvatar.view', methods=["GET"])
+def endpoint_get_avatar() -> flask.Response:
+    username = flask.request.args.get('username', default='', type=safe_str)    # Required
+    if not username:
+        return subsonic_error(10)
+
+    blob, last_changed = get_user_avatar(username)
+    if not blob:
+        flask.abort(404)
+
+    mimetype = sniff_image(blob) or 'image/jpeg'
+
+    response = flask.make_response(blob)
+    response.headers.set('Content-Type', mimetype)
+    response.headers.set('Cache-Control', 'public, max-age=86400')  # 1 day cached
+    return response
